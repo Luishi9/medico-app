01a0e5f9eea038733243fcfa586b79780724e273
Luis Eduardo <luisrubin977@gmail.com>
2025-04-24 13:01:17 -0600

    router-link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, LoginComponent],
-  templateUrl: './login/login.component.html',
-
-  /*template: `
-    <h1>Medico App</h1>
-    
-    <app-login></app-login>
-  `,
-  */
-
-  styleUrl: './login/login.component.css'
+  imports: [CommonModule, RouterOutlet, RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'medico-app';
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,40 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  
+  imports: [
+    FormsModule,
+    BrowserModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  /*
+  usuario: string = '';
+  contrasena: string = '';
+
+  constructor(private authService: AuthService) { }
+
+  login() {
+
+    this.authService.login(this.usuario, this.contrasena).subscribe((res: any) => {
+      if (res.success) {
+        alert("Login correcto");
+      } else {
+        alert("Login incorrecto");
+      }
+    }, error => {
+      console.error('Error de conexio', error);
+      alert('Hubo un problema a conectar con el servidor');
+    });
+  }
+    */
 
 }

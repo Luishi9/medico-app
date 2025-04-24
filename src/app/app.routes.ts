4e9cb7bc1d74a6a51eaeb760fc90ce5a078e6eb8
Luis Eduardo <luisrubin977@gmail.com>
2025-04-24 15:13:03 -0600

    login-listo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'inicio', component: InicioComponent },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,19 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-login',
-  
-  imports: [
-    FormsModule,
-    BrowserModule
-  ],
+  standalone: true,
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  /*
+  
   usuario: string = '';
   contrasena: string = '';
 
@@ -26,15 +23,14 @@ export class LoginComponent {
 
     this.authService.login(this.usuario, this.contrasena).subscribe((res: any) => {
       if (res.success) {
-        alert("Login correcto");
+        alert(res.message);
       } else {
-        alert("Login incorrecto");
+        alert(res.message);
       }
     }, error => {
       console.error('Error de conexio', error);
       alert('Hubo un problema a conectar con el servidor');
     });
   }
-    */
 
 }
